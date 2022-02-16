cedd6b4ccec0c043af91182c7982d11ea6efeb26
felipemaxrosa <felipemax.suporteti@gmail.com>
2022-02-16 14:08:43 -0300

    split environments

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./styles.scss";
 export const App = () => {
   return (
     <>
-      <h1>React TypeScript Webpack Starter Template</h1>
+      <h1>React TypeScript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}</h1>
       <img src={IMAGE} alt="React Logo" />
       <img src={SVG} alt="React Logo" />
     </>
