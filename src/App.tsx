147328ede541fe03c3cab6d6a1007c6b29211581
felipemaxrosa <felipemax.suporteti@gmail.com>
2022-02-20 17:33:04 -0300

    Test husky

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import ClickCounter from './components/ClickCounter';
 import './styles.scss';
 
 export const App = () => {
+  const name = 'Felipe';
   return (
     <>
       <h1>
-        React TypeScript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}
+        React TypeScript Webpack Starter Template - {process.env.NODE_ENV} {name}
       </h1>
       <img src={IMAGE} alt="React Logo" width="300" height="200" />
       <img src={SVG} alt="React Logo" width="300" />
