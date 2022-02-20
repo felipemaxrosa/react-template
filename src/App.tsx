47b21f2c97fa476ad330517064e950d9b51cd148
felipemaxrosa <felipemax.suporteti@gmail.com>
2022-02-20 17:34:08 -0300

    Test husky reverted

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,10 @@ import ClickCounter from './components/ClickCounter';
 import './styles.scss';
 
 export const App = () => {
-  const name = 'Felipe';
   return (
     <>
       <h1>
-        React TypeScript Webpack Starter Template - {process.env.NODE_ENV} {name}
+        React TypeScript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}
       </h1>
       <img src={IMAGE} alt="React Logo" width="300" height="200" />
       <img src={SVG} alt="React Logo" width="300" />
