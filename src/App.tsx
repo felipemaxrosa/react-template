797ccc88b9bb6178889ad7c827076531f71491ed
felipemaxrosa <felipemax.suporteti@gmail.com>
2022-02-20 17:48:17 -0300

    feat: CLEANING CODE

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import IMAGE from './assets/react-icon.png';
-import SVG from './assets/bookmark.svg';
-import ClickCounter from './components/ClickCounter';
 import './styles.scss';
 
 export const App = () => {
   return (
-    <>
-      <h1>
-        React TypeScript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}
-      </h1>
-      <img src={IMAGE} alt="React Logo" width="300" height="200" />
-      <img src={SVG} alt="React Logo" width="300" />
-      <ClickCounter />
-    </>
+    <h1>
+      React TypeScript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}
+    </h1>
   );
 };
