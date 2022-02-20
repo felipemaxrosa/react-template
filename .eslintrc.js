2ee4157d974a5a5e4bd441ca0f77f9e58335b593
felipemaxrosa <felipemax.suporteti@gmail.com>
2022-02-20 17:21:00 -0300

    feat: PRETTIER configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,10 +23,11 @@ module.exports = {
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'prettier/prettier': ['off', { endOfLine: 'auto' }],
   },
   settings: {
     react: {
       version: 'detect',
     },
   },
-}
+};
