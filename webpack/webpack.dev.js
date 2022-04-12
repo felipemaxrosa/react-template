d63bb13441882c9745c9a61a02bb4b6a21c5a013
Israel Antonio Rosales Laguan <israel.laguan@equinoxmedia.com>
2022-04-12 01:21:50 -0500

    feat: add js minification (#13)
    * feat: add js minification
    
    * fix: indentation error
    
    * fix: indentation error
    
    * fix: indentation error
    
    * fix: remove invalid option in github action
    
    * fix: remove bundle server in CI/CD
    
    * fix: avoid to open bundle server
    
    * feat: move build analyser to another action
    
    * fix: remove extra step
    
    * fix: lockfile updated
    
    * fix: remove extra option

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -2,16 +2,18 @@ const webpack = require("webpack");
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 module.exports = {
-	mode: "development",
-	devServer: {
-		hot: true,
-		open: true,
-	},
-	devtool: "cheap-module-source-map",
-	plugins: [
-		new webpack.DefinePlugin({
-			"process.env.name": JSON.stringify("FelipeRosaDev"),
-		}),
-		new ReactRefreshWebpackPlugin(),
-	],
+  mode: 'development',
+  devServer: {
+    hot: true,
+    open: true,
+    port: 3000,
+    writeToDisk: false, // https://webpack.js.org/configuration/dev-server/#devserverwritetodisk-
+  },
+  devtool: 'cheap-module-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.name': JSON.stringify('FelipeRosaDev'),
+    }),
+    new ReactRefreshWebpackPlugin(),
+  ],
 };
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,13 +1,24 @@
 const webpack = require("webpack");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-	mode: "production",
-	devtool: "source-map",
-	plugins: [
-		new webpack.DefinePlugin({
-			"process.env.name": JSON.stringify("FelipeRosaProd"),
-		}),
-		new BundleAnalyzerPlugin()
-	],
+  mode: 'production',
+  devtool: 'source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.name': JSON.stringify('FelipeRosaProd'),
+    }),
+    new BundleAnalyzerPlugin({ openAnalyzer: false, analyzerMode: 'static' }),
+  ],
+  // how to write the compiled files to disk
+  // https://webpack.js.org/concepts/output/
+  output: {
+    filename: '[name].[hash:20].js',
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin(), new OptimizeCssAssetsPlugin({})],
+  },
 };
