472286105615383f7e1df80151b318c5432d5763
Felipe Rosa <felipemax.suporteti@gmail.com>
2022-04-14 21:20:53 -0300

    feat: jest config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  transform: {
+    '^.+\\.(t|j)sx?$': 'ts-jest',
+  },
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  testEnvironment: 'jsdom',
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './styles.scss';
 
-export const App = () => {
+const App = () => {
   return (
     <h1>
       React TypeScript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}
     </h1>
   );
 };
+
+export default App;
